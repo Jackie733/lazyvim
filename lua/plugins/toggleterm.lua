446119d6feeca9cd2a08799ba62eeaa9fa0a996f
Jackie Tsien <jackie10673311@gmail.com>
2023-06-25 21:58:41 +0800

    update toggleterm,delete tailwind config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,20 +5,32 @@ return {
     config = true,
     keys = {
       {
-        "<F4>",
-        "<cmd>ToggleTerm<cr>",
-        desc = "Toggle floating terminal",
+        [[<C-\>]],
+      },
+      {
+        "<leader>0",
+        "<Cmd>2ToggleTerm<Cr>",
+        desc = "Terminal #2",
       },
     },
+    cmd = { "ToggleTerm", "TermExec" },
     opts = {
-      open_mapping = [[<F4>]],
+      open_mapping = [[<C-\>]],
       direction = "float",
       shade_filetypes = {},
+      shade_terminals = false,
+      shading_factor = 0.3,
       hide_numbers = true,
       insert_mappings = true,
       terminal_mappings = true,
       start_in_insert = true,
       close_on_exit = true,
+      winbar = {
+        enable = false,
+        name_formatter = function(term)
+          return term.name
+        end,
+      },
     },
   },
 }
