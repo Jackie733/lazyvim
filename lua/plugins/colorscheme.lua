48a5ce6c26c40b19c53658cf7964bcc2cc3d4425
Jackie Tsien <jackie10673311@gmail.com>
2024-03-12 17:55:59 +0800

    change colorschema

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "shaunsingh/nord.nvim",
+    "AlexvZyl/nordic.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd("colorscheme nord")
+      require("nordic").load()
     end,
   },
 }
