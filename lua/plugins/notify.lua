f272a4ae45b8179c764dd1f102a640251f258e99
Jackie Tsien <jackie10673311@gmail.com>
2024-03-18 18:28:05 +0800

    delete some plugins

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,16 +1,4 @@
 return {
-  -- "rcarriga/nvim-notify",
-  -- config = function()
-  --   local banned_messages = { "No information available" }
-  --   vim.notify = function(msg, ...)
-  --     for _, banned in ipairs(banned_messages) do
-  --       if msg == banned then
-  --         return
-  --       end
-  --     end
-  --     return require("notify")(msg, ...)
-  --   end
-  -- end,
   "folke/noice.nvim",
   opts = function(_, opts)
     table.insert(opts.routes, {
